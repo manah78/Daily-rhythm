0e95d96528eb544cbda7f58470c2fd8107ff2571
manah78 <45176256+manah78@users.noreply.github.com>
2026-09-22 08:30:03 -0400

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'daily-rhythm-v1.0.0-my-prayer-v14';
+const CACHE_VERSION = 'daily-rhythm-v1.0.0-phase1-voice-v16';
 const APP_CACHE = `${CACHE_VERSION}-app`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
